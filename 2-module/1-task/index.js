41e59ca032ea4881c72757801ef82d4b8b8866e9
Oleg Lukyanov <lukyanovoa@yandex.ru>
2019-12-16 16:19:31 +0300

    2-module done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,7 +1,19 @@
 /**
  * Клонируем объект
  * @param {Object} obj - клонируем объект
- * @returns {Object}
+ * @returns {Object} 
  */
 function clone(obj) {
+  if (obj === null) {
+    return obj;
+  }
+  let obj2 = {};
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      obj2[key] = clone(obj[key]);
+    } else {
+      obj2 = Object.assign({}, obj);
+    }
+  }	 
+  return obj2;
 }
